1f77ba4e5ccca983f89d9604db77f600d33db555
Sam <shortdriva@aol.com>
2024-04-12 12:58:01 -0400

    Initial commit

---
+++ app/src/main/java/com/samschu/cs4520/weather/ui/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.samschu.cs4520.weather.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
