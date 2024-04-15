7e9f4c49997680a8be5d2265a94d14682aed013e
Sam <shortdriva@aol.com>
2024-04-14 23:24:38 -0400

    Update new location dropdown to use array resource
    Location selection dropdown now pulls items from string array resource

--- app/src/main/java/com/samschu/cs4520/weather/ui/LocationScreen.kt
+++ app/src/main/java/com/samschu/cs4520/weather/ui/LocationScreen.kt
@@ -20,15 +20,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.samschu.cs4520.weather.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LocationScreen() {
-    var newLocation by remember { mutableStateOf("default") }
+    val locationOptions = stringArrayResource(R.array.location_options)
+
+    var newLocation by remember { mutableStateOf(locationOptions.first()) }
     var dropdownExpanded by remember { mutableStateOf(false) }
 
     Column(
@@ -70,13 +74,15 @@ fun LocationScreen() {
                 expanded = dropdownExpanded,
                 onDismissRequest = { dropdownExpanded = false }
             ) {
-                DropdownMenuItem(
-                    text = { Text(text = "1") },
-                    onClick = {
-                        newLocation = "1"
-                        dropdownExpanded = false
-                    }
-                )
+                locationOptions.forEach {
+                    DropdownMenuItem(
+                        text = { Text(text = it) },
+                        onClick = {
+                            newLocation = it
+                            dropdownExpanded = false
+                        }
+                    )
+                }
             }
         }
         Spacer(modifier = Modifier.height(10.dp))
